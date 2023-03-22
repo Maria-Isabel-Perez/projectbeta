f10cab9344f2f114dd103ee7e09754fc28f06f75
Maria-Isabel-Perez <mariaisabelperezdavila@hotmail.com>
2023-03-22 11:52:47 +0100

    PROJECT-BETA-HEADER

---
+++ front-page.php
@@ -0,0 +1,11 @@
+<!-- Calendly link widget begin 
+<link href="https://assets.calendly.com/assets/external/widget.css" rel="stylesheet">
+<script src="https://assets.calendly.com/assets/external/widget.js" type="text/javascript" async></script>
+<a href="" onclick="Calendly.initPopupWidget({url: 'https://calendly.com/mariaisabelperez/15min?primary_color=b27cf0'});return false;">¡Agenda aquí tu asesoría gratuita!</a>
+Calendly link widget end -->
+
+<?php get_header(); ?>
+
+
+
+<?php get_footer(); ?>
---
+++ header.php
@@ -0,0 +1,27 @@
+<html <?php language_attributes(); ?>>
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="theme-color" content="#FFFFFF">
+
+        <?php wp_head(); ?>
+
+    <body>
+    <div class=header>
+        
+        <img class="header__logo" src="<?php bloginfo ( 'template_url' ); ?>/img/basis-visuals-logo" alt="Logo Basis Visuals">
+        
+    
+    
+    </div>
+
+
+    </body>
+
+
+    </head>
+
+
+        
